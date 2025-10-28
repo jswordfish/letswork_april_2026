5eaa29d9fc57cf4040b519754d8ebfde47c8bef4
User <User@DESKTOP-8U304IJ>
2025-10-28 04:40:29 -0700

    LetsWork Centre time format

--- src/main/java/com/letswork/crm/entities/LetsWorkCentre.java
+++ src/main/java/com/letswork/crm/entities/LetsWorkCentre.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,12 +43,20 @@ public class LetsWorkCentre extends Base{
     
     private String amenities;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime startTimeRegular;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime endTimeRegular;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime startTimeSat;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime endTimeSat;
+    
+    private String latitude;
+    
+    private String longitude;
 
 }
