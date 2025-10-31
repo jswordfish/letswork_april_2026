a2ff0bee1140a126a29e6633b5d2fa7d3670391e
User <User@DESKTOP-8U304IJ>
2025-10-31 04:44:34 -0700

    Improves api names corrected

--- src/main/java/com/letswork/crm/controller/WifiRouterMappingToClientController.java
+++ src/main/java/com/letswork/crm/controller/WifiRouterMappingToClientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import com.letswork.crm.service.WifiRouterMappingToClientService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/wifiRouterMappingToClient")
 public class WifiRouterMappingToClientController {
 	
 	@Autowired
