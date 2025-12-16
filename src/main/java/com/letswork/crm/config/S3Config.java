224408bcf07296f86568e4b9495946c00384e912
hp <hp@JANI>
2025-12-16 18:12:48 +0530

    S3 URL improved for frontend

--- src/main/java/com/letswork/crm/config/S3Config.java
+++ src/main/java/com/letswork/crm/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -13,20 +14,29 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
+	@Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(
+                        "AKIAV4XIACUNK2STTVGS",
+                        "10DBzNXOPsHkw472gpr1jjffSLSutYQz3oehxCOX"
+                )
+        );
+    }
+
     @Bean
-    public S3Client s3Client() {
+    public S3Client s3Client(AwsCredentialsProvider credentialsProvider) {
         return S3Client.builder()
-                .region(Region.AP_SOUTH_1) // change region as per your bucket
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("AKIAV4XIACUNK2STTVGS", "10DBzNXOPsHkw472gpr1jjffSLSutYQz3oehxCOX")
-                ))
+                .region(Region.AP_SOUTH_1)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
-    
+
     @Bean
-    public S3Presigner s3Presigner() {
+    public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider) {
         return S3Presigner.builder()
                 .region(Region.AP_SOUTH_1)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
     
